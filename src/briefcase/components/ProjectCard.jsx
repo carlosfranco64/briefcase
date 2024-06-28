f711b1e4eae000051ebacd1b39c800e115bf84d7
carlosfranco <carlosfranco6456@gmail.com>
2024-06-28 12:24:45 -0500

    card

--- src/briefcase/components/ProjectCard.jsx
+++ src/briefcase/components/ProjectCard.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useAll } from "../../context/AllContext";
+import { Button } from "@headlessui/react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { IoEyeSharp } from "react-icons/io5";
 
 export const ProjectCard = ({ project }) => {
 
@@ -9,7 +12,6 @@ const {isMode}=useAll()
   return (
     <div>
       <div className="relative group overflow-hidden  rounded-lg shadow-lg hover:shadow-xl transition-transform duration-300 ease-in-out hover:-translate-y-2">
-        <Link to="/" className="absolute inset-0 z-10" />
         <img
           src={`${project.img}`}
           alt=""
@@ -26,13 +28,36 @@ const {isMode}=useAll()
             } text-justify`}>
             {project.description}
           </p>
-          <div className="mt-4">    
-            <Link
+          <div className="mt-4 flex gap-2">    
+            <Button
               to="/"
-              className="inline-block bg-blue-500 hover:bg-gray-800 text-white px-4 py-2 rounded-md text-sm font-medium shadow-md transition-colors focus:outline-none focus:ring-2 focus:ring-gray-900"
+              className="inline-block bg-blue-500 hover:bg-gray-800 text-white px-4 py-1 rounded-md text-xl font-medium shadow-md transition-colors focus:outline-none focus:ring-2 focus:ring-gray-900"
             >
-              Ver más
-            </Link>
+              <IoEyeSharp />
+            </Button>
+
+
+
+         
+          <a
+            href="https://github.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="my-auto"
+          >
+            <FaGithub
+              className={` ${
+                isMode
+                  ? "text-gray-700"
+                  : "text-white bg-gray-700 hover:text-gray-900"
+              } transition duration-300 rounded-full`}
+              size={30}
+            />
+          </a>
+
+
+
+
           </div>
         </div>
       </div>
--- src/briefcase/pages/Projets.jsx
+++ src/briefcase/pages/Projets.jsx
@@ -55,7 +55,7 @@ export const Projets = () => {
         <div className="mb-8 md:mb-12">
           <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl text-center text-gray-800">Mis Proyectos</h2>
         </div>
-        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 ">
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-2 ">
 {
   projects.map((project,i)=>(
 
