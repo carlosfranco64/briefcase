4982995afeb74e7496a69619e39567a4df3c1757
carlosfranco <carlosfranco6456@gmail.com>
2024-07-03 22:56:56 -0500

    ajustes de letras

--- src/briefcase/pages/Projets.jsx
+++ src/briefcase/pages/Projets.jsx
@@ -57,7 +57,9 @@ export const Projets = () => {
       <Header />
       <div className={`container px-4 md:px-6 py-12 grid ${isOpen ? "ml-0" : "ml-0 md:ml-0 "}  `}> {/* scroll horizoltal */}
         <div className="mb-8 md:mb-12">
-          <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl text-center text-gray-800">Mis Proyectos</h2>
+          <h2 className={`text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl text-center ${
+          isMode ? "text-gray-700" : "text-white"
+        }`}>Mis Proyectos</h2>
         </div>
         <div className="grid grid-cols-1 gap-6  sm:grid-cols-2 lg:grid-cols-3  ">
 {
